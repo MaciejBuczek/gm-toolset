6ecfc3a8c4c504edf4c106b11e61b19e3c249fc8
Maciej Buczek <maciejbuczek98@gmail.com>
2026-08-03 13:15:46 +0200

    Moved app info api to common project. Updated open api nuget packages

---
+++ src/Common/Common/SharedEndpoints/AppInfo.cs
@@ -0,0 +1,32 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Reflection;
+
+namespace Common.SharedEndpoints
+{
+    public class AppInfo : ICarterModule
+    {
+        public record AppInfoResponse(
+            string ApiName,
+            string Environment
+            );
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("app-info", () =>
+            {
+                var appInfo = new AppInfoResponse(
+                    ApiName: Assembly.GetEntryAssembly()?.GetName().Name ?? SharedEndpointConstants.DefaultApiName,
+                    Environment: Environment.GetEnvironmentVariable(SharedEndpointConstants.EnvironmentVariableName) ?? SharedEndpointConstants.DefaultEnvironment
+                );
+                return Results.Ok(appInfo);
+            })
+            .WithName("App Info")
+            .WithTags("SharedEndpoints")
+            .Produces<AppInfoResponse>(StatusCodes.Status200OK)
+            .WithOpenApi();
+        }
+    }
+}
---
+++ src/Common/Common/SharedEndpoints/SharedEndpointConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.SharedEndpoints
+{
+    public static class SharedEndpointConstants
+    {
+        public const string DefaultApiName = "Unknown";
+        public const string DefaultEnvironment = "Unknown";
+        public const string EnvironmentVariableName = "ASPNETCORE_ENVIRONMENT";
+    }
+}
