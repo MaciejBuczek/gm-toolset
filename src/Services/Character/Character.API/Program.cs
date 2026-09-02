6906a06891f2beb3bfae61469b175718ecaf01e9
Maciej Buczek <maciejbuczek98@gmail.com>
2026-09-02 22:43:52 +0200

    Refactor DI setup into DependencyInjectionSetup.cs
    The dependency injection logic was refactored to improve organization and maintainability. The `DependencyInjection.cs` file was removed, and its functionality was moved to `DependencyInjectionSetup.cs`. A new `SetUpDI` extension method was introduced to centralize service registrations, including Marten connection, repositories, request handlers, validators, exception handling, identity, and Carter.
    
    In `Program.cs`, service registration was simplified by replacing individual method calls with the `SetUpDI` method. Additional private extension methods were added in `DependencyInjectionSetup.cs` to encapsulate specific service setup logic.
    
    These changes enhance code readability and consolidate DI logic into a single, reusable method.

--- src/Services/Character/Character.API/Program.cs
+++ src/Services/Character/Character.API/Program.cs
@@ -4,16 +4,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddJwtSwaggerGen();
-
-builder.Services.AddMartenConnection(builder.Configuration);
-builder.Services.AddCharacterRepository();
-builder.Services.AddHandlers();
-builder.Services.AddCarter();
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
-builder.Services.AddAuthorization();
-builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.SetUpDI(builder.Configuration);
 
 var app = builder.Build();
 
