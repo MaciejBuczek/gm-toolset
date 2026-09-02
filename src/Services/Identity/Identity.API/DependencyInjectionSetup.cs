8c1a32131793aa19c459fb9261588fbb88f5608a
Maciej Buczek <maciejbuczek98@gmail.com>
2026-09-02 22:15:19 +0200

    Refactor DI setup and consolidate service registration
    Refactored `DependencyInjectionSetup` to centralize and streamline dependency injection logic. Removed `ServiceCollectionsExtensions` and migrated its functionality into `DependencyInjectionSetup`. Simplified `Program.cs` by replacing multiple service setup calls with a single `SetUpDI` method.
    
    Introduced a `ConnectionStrings` class for managing database connection strings. Enhanced `DependencyInjectionSetup` with helper methods for adding services, configuring options, and setting up the identity context.
    
    Removed redundant code and unused global usings to improve maintainability and readability.

---
+++ src/Services/Identity/Identity.API/DependencyInjectionSetup.cs
@@ -0,0 +1,61 @@
+﻿namespace Identity.API
+{
+    internal static class DependencyInjectionSetup
+    {
+        internal static IServiceCollection SetUpDI(this IServiceCollection services, ConfigurationManager configurationManager)
+        {
+            var assembly = typeof(DependencyInjectionSetup).Assembly;
+            services
+                 .AddValidators(assembly)
+                 .AddExceptionHandler<CustomExceptionHandler>()
+                 .AddMessaging(configurationManager)
+                 .AddMessagingHandlers()
+                 .AddRequestHandlers(assembly)
+                 .DecorateRequestWithEventHandling()
+                 .DecorateRequestWithValidation()
+                 .DecorateRequestWithLogging()
+                 .AddCarter()
+                 .SetupOptions(configurationManager)
+                 .SetupIdentityContext(configurationManager)
+                 .AddServices();
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<DbSetupHelper>();
+            services.AddScoped<ITokenGeneratorService, TokenGeneratorService>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddScoped<IIdentityService, IdentityService>();
+
+            return services;
+        }
+
+        public static IServiceCollection SetupIdentityContext(this IServiceCollection services, ConfigurationManager configurationManager)
+        {
+            var dbOptions = configurationManager.GetSection(nameof(ConnectionStrings)).Get<ConnectionStrings>();
+            var connectionString = dbOptions?.Database?? string.Empty;
+
+            if(connectionString != string.Empty)
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(connectionString, options =>
+                {
+                    options.MigrationsHistoryTable(Constants.DefaultMigrationsHistoryTable, Constants.DefaultSchema);
+                }));
+                services.AddIdentity<AppUser, IdentityRole<Guid>>()
+                    .AddEntityFrameworkStores<AppDbContext>()
+                    .AddDefaultTokenProviders();
+            }          
+
+            return services;
+        }
+
+        private static IServiceCollection SetupOptions(this IServiceCollection services, ConfigurationManager configurationManager)
+        {
+            return services.Configure<Jwt>(configurationManager.GetSection(nameof(Jwt)))
+                .Configure<ConnectionStrings>(configurationManager.GetSection(nameof(ConnectionStrings)));
+        }
+    }
+}
